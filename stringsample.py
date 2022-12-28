bad958e727b3590dfe84e1d6e0f92a46d1250e79
Thamo <thamotharanm97@gmail.com>
2022-12-28 16:26:36 +0530

    added stringsample.py file

---
+++ stringsample.py
@@ -0,0 +1,11 @@
+# below is the example to assign the string to the variable
+
+name = 'thamo'
+
+print(name)
+
+# below is the example to receive the input from the users
+
+input_name = input('what is your name : ')
+
+print(f'{input_name} who learns python')
