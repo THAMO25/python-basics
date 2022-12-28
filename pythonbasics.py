40b79b64320da8e423ebffd104eb184cf4993992
Thamo <thamotharanm97@gmail.com>
2022-12-28 19:10:53 +0530

    added stringsample.py file

---
+++ pythonbasics.py
@@ -0,0 +1,10 @@
+# below is the example for print the string in python
+
+print('hello world')
+
+print('0-----')
+print(' ||||')
+
+# python will consider the below statement that string(*) to be printed 10 times
+
+print('*' * 10)
